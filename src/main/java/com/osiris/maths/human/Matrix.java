255dc4b24b39949d439270629a367406a709eaa5
Osiris Team <osiris_support@pm.me>
2022-04-22 11:25:57 +0200

    work

---
+++ src/main/java/com/osiris/maths/human/Matrix.java
@@ -0,0 +1,218 @@
+package com.osiris.maths.human;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Matrix {
+    public int rowsCount, columnsCount;
+    public List<List<Double>> rows;
+
+    /**
+     * Creates a matrix filled with zeros, with
+     * the provided row and column counts. <br>
+     * Example with rowCount=3 and columnCount=2: <br>
+     * [0.0, 0.0] <br>
+     * [0.0, 0.0] <br>
+     * [0.0, 0.0] <br>
+     * Note that quadratic matrices are needed for most calculations. <br>
+     */
+    public Matrix(int rowsCount, int columnsCount) {
+        this.rowsCount = rowsCount;
+        this.columnsCount = columnsCount;
+        this.rows = new ArrayList<>();
+        for (int i = 0; i < rowsCount; i++) {
+            List<Double> values = new ArrayList<>(columnsCount);
+            Collections.fill(values, 0.0);
+            this.rows.add(values);
+        }
+    }
+
+    /**
+     * Returns the value at the provided row and column index.
+     * @param iR indexRow
+     * @param iC indexColumn
+     */
+    public Double at(int iR, int iC){
+        return rows.get(iR).get(iC);
+    }
+
+    public void resize(int newRowsCount, int newColumnsCount){
+        if(newRowsCount > rowsCount){
+            for (int i = 0; i < newRowsCount - rowsCount; i++) {
+                List<Double> values = new ArrayList<>();
+                for (int j = 0; j < newColumnsCount; j++) {
+                    values.add(0.0);
+                }
+                this.rows.add(values);
+            }
+        } else{
+            for (int i = 0; i < rowsCount - newRowsCount; i++) {
+                this.rows.remove(rowsCount-1-i);
+            }
+        }
+        if(newColumnsCount > columnsCount){
+            for (int i = 0; i < newColumnsCount - columnsCount; i++) {
+                for (List<Double> row :
+                        rows) {
+                    if(row.size() < newColumnsCount)
+                        row.add(0.0);
+                }
+            }
+        } else{
+            for (int i = 0; i < columnsCount - newColumnsCount; i++) {
+                for (List<Double> row :
+                        rows) {
+                    row.remove(row.size()-1);
+                }
+            }
+        }
+        this.columnsCount = newColumnsCount;
+    }
+
+    public void setRow(int i, Double... values){
+        List<Double> list = new ArrayList<>(values.length);
+        Collections.addAll(list, values);
+        setRow(i, list);
+    }
+    public void setRow(int i, List<Double> values){
+        this.rows.set(i, values);
+    }
+
+    public List<List<Double>> getColumns(){
+        List<List<Double>> columns = new ArrayList<>(columnsCount);
+        for (int i = 0; i < columnsCount; i++) {
+            List<Double> column = new ArrayList<>(rows.size());
+            columns.add(column);
+            for (List<Double> row : rows) {
+                column.add(row.get(i));
+            }
+        }
+        return columns;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "  "+rowsCount+"x"+columnsCount+ "\n" + asString();
+    }
+
+    public String asString(){
+        return asString(rows);
+    }
+
+    public String asString(List<List<Double>> list){
+        StringBuilder s = new StringBuilder();
+        for (List<Double> row :
+                list) {
+            s.append(row+"\n");
+        }
+        return s.toString();
+    }
+
+    public class NotEqual extends Exception{
+        public NotEqual(String message){
+            super(message);
+        }
+        public NotEqual(Matrix m0, Matrix m1) {
+            super("Both matrices must have the same row and column count. m0="+m0.rowsCount
+                    +"x"+m0.columnsCount+" != m1="+m1.rowsCount+"x"+m1.columnsCount);
+        }
+    }
+
+    /**
+     * @return a new matrix containing the result.
+     */
+    public Matrix add(Matrix m) throws NotEqual {
+        if(rowsCount!=m.rowsCount || columnsCount!=m.columnsCount)
+            throw new NotEqual(this, m);
+        Matrix newM = new Matrix(m.rowsCount, m.columnsCount);
+        for (int i = 0; i < m.rows.size(); i++) {
+            List<Double> row = rows.get(i);
+            List<Double> otherRow = m.rows.get(i);
+            List<Double> newRow = new ArrayList<>(row.size());
+            newM.setRow(i, newRow);
+            for (int j = 0; j < row.size(); j++) {
+                newRow.add(row.get(j) + otherRow.get(j));
+            }
+        }
+        return newM;
+    }
+
+    /**
+     * @return a new matrix containing the result.
+     */
+    public Matrix subtract(Matrix m) throws NotEqual {
+        if(rowsCount!=m.rowsCount || columnsCount!=m.columnsCount)
+            throw new NotEqual(this, m);
+        Matrix newM = new Matrix(m.rowsCount, m.columnsCount);
+        for (int i = 0; i < m.rows.size(); i++) {
+            List<Double> row = rows.get(i);
+            List<Double> otherRow = m.rows.get(i);
+            List<Double> newRow = new ArrayList<>(row.size());
+            newM.setRow(i, newRow);
+            for (int j = 0; j < row.size(); j++) {
+                newRow.add(row.get(j) - otherRow.get(j));
+            }
+        }
+        return newM;
+    }
+
+    /**
+     * Multiplies this matrix with the provided number.
+     * @return a new matrix containing the result.
+     */
+    public Matrix multiply(Double num){
+        Matrix newM = new Matrix(rowsCount, columnsCount);
+        for (int i = 0; i < rows.size(); i++) {
+            List<Double> row = rows.get(i);
+            List<Double> newRow = new ArrayList<>(row.size());
+            newM.setRow(i, newRow);
+            for (Double d : row) {
+                newRow.add(d * num);
+            }
+        }
+        return newM;
+    }
+
+    /**
+     * Multiplies this matrix with the provided matrix. <br>
+     * Also called scalar matrix multiplication. <br>
+     * @return a new matrix containing the result.
+     * @throws NotEqual if the provided matrix row count is not equal to the current matrix column count.
+     */
+    public Matrix multiply(Matrix m) throws NotEqual {
+        if(m.rowsCount!=columnsCount)
+            throw new NotEqual("Provided matrix rowCount must be equal to this matrix columnCount. "+m.rowsCount+"!="+columnsCount);
+        Matrix newM = new Matrix(rowsCount, m.columnsCount);
+        List<List<Double>> otherColumns = m.getColumns();
+        for (int i = 0; i < rowsCount; i++) {
+            List<Double> row = rows.get(i);
+            List<Double> newRow = new ArrayList<>(otherColumns.size());
+            newM.setRow(i, newRow);
+            for (List<Double> otherColumn : otherColumns) {
+                double result = 0.0;
+                for (int k = 0; k < row.size(); k++) {
+                    result += row.get(k) * otherColumn.get(k);
+                }
+                newRow.add(result);
+            }
+        }
+        return newM;
+    }
+
+
+    public Double determinant(){
+        return null;
+    }
+
+    public Matrix laplaceExpansion(){
+        return null;
+    }
+
+    public class NotQuadratic extends Exception{
+        public NotQuadratic(Matrix m) {
+            super("Matrix must be quadratic (rows count == columns count) to perform this operation! "+m.rowsCount+"!="+m.columnsCount);
+        }
+    }
+}
---
+++ src/test/java/com/osiris/maths/human/MatrixTest.java
@@ -0,0 +1,124 @@
+package com.osiris.maths.human;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MatrixTest {
+    Matrix m2x2 = new Matrix(2, 2);
+    Matrix m5x5 = new Matrix(5, 5);
+    public MatrixTest() {
+        m2x2.setRow(0, 5.0, 2.0);
+        m2x2.setRow(1, 3.0, 1.0);
+
+        m5x5.setRow(0, 10.2, 2.0, 22.1, 4.90, 19.2);
+        m5x5.setRow(1, 10.2, 1.0, 22.1, 4.90, 19.2);
+        m5x5.setRow(2, 10.2, 0.0, 22.1, 4.90, 19.2);
+        m5x5.setRow(3, 10.2, 5.0, 22.1, 4.90, 19.);
+        m5x5.setRow(4, 10.2, 6.0, 22.1, 4.90, 19.2);
+    }
+
+    @Test
+    void getColumns() {
+        System.out.println(m5x5.asString());
+        List<List<Double>> columns = m5x5.getColumns();
+        String[] rows = new String[columns.size()];
+        Arrays.fill(rows, "");
+        for (List<Double> column : columns) {
+            for (int j = 0; j < rows.length; j++) {
+                rows[j] = rows[j] + column.get(j) + " ";
+            }
+        }
+        for (String row :
+                rows) {
+            System.out.println(row);
+        }
+        System.out.println(m5x5.asString(columns));
+    }
+
+    @Test
+    void asString() {
+    }
+
+    @Test
+    void testAsString() {
+    }
+
+    @Test
+    void add() throws Matrix.NotEqual {
+        Matrix m = new Matrix(2, 2);
+        m.setRow(0, 1.0, 2.0);
+        m.setRow(1, 1.0, 2.0);
+        m = m.add(m);
+        assertEquals(2.0, m.rows.get(0).get(0));
+        assertEquals(4.0, m.rows.get(0).get(1));
+        assertEquals(2.0, m.rows.get(1).get(0));
+        assertEquals(4.0, m.rows.get(1).get(1));
+    }
+
+    @Test
+    void subtract() throws Matrix.NotEqual {
+        Matrix m = new Matrix(2, 2);
+        m.setRow(0, 1.0, 2.0);
+        m.setRow(1, 1.0, 2.0);
+        m = m.subtract(m);
+        assertEquals(0.0, m.rows.get(0).get(0));
+        assertEquals(0.0, m.rows.get(0).get(1));
+        assertEquals(0.0, m.rows.get(1).get(0));
+        assertEquals(0.0, m.rows.get(1).get(1));
+    }
+
+    @Test
+    void determinant() {
+    }
+
+    @Test
+    void laplaceExpansion() {
+    }
+
+    @Test
+    void at() {
+    }
+
+    @Test
+    void resize() {
+        Matrix m = new Matrix(2, 2);
+        m.setRow(0, 0.0, 3.0);
+        m.setRow(1, 1.0, 2.0);
+        m.resize(4, 4);
+        assertEquals(4, m.rows.size());
+        assertEquals(4, m.getColumns().size());
+    }
+
+    @Test
+    void setRow() {
+    }
+
+    @Test
+    void testSetRow() {
+    }
+
+    @Test
+    void multiply() throws Matrix.NotEqual {
+        Matrix result = m2x2.multiply(2.0);
+        assertEquals(10.0, result.at(0,0));
+        assertEquals(4.0, result.at(0,1));
+        assertEquals(6.0, result.at(1,0));
+        assertEquals(2.0, result.at(1,1));
+    }
+
+    @Test
+    void testMultiply() throws Matrix.NotEqual {
+        Matrix m = new Matrix(2, 2);
+        m.setRow(0, 0.0, 3.0);
+        m.setRow(1, 1.0, 2.0);
+        Matrix result = m2x2.multiply(m);
+        assertEquals(2.0, result.at(0,0));
+        assertEquals(19.0, result.at(0,1));
+        assertEquals(1.0, result.at(1,0));
+        assertEquals(11.0, result.at(1,1));
+    }
+}
